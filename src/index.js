e9ad8bf00da9fa79026db479416d2755e7b8a219
Florian Hines <syn@ronin.io>
2017-10-12 21:52:10 -0500

    wat

--- src/index.js
+++ src/index.js
@@ -6,74 +6,80 @@
  */
 
 import {log, warn} from './utils/console';
-import amplitude from 'amplitude-js';
 
 var _debug = false;
 
-const Amplitude = {
-  initialize: function (amplitudeTrackingCode) {
-    if (!amplitudeTrackingCode) {
-      warn('amplitudeTrackingCode is required in initialize()');
-      return;
-    }
+const __ENV__ = process.env.NODE_ENV || 'development'; // eslint-disable-line no-underscore-dangle
 
-    amplitude.getInstance().init(amplitudeTrackingCode);
-  },
-
-  /**
-   * amplitude:
-   * Returns the original Amplitude object.
-   */
-  amplitude: function () {
-    if (arguments.length > 0) {
-      amplitude.apply(this, arguments);
-      if (_debug) {
-        log('called amplitude(\'arguments\');');
-        log('with arguments: ' + JSON.stringify([].slice.apply(arguments)));
+if (__ENV__ !== 'test') {
+  let amplitude = require('amplitude-js');
+  
+  const Amplitude = {
+    initialize: function (amplitudeTrackingCode) {
+      if (!amplitudeTrackingCode) {
+        warn('amplitudeTrackingCode is required in initialize()');
+        return;
       }
-      return;
+  
+      amplitude.getInstance().init(amplitudeTrackingCode);
+    },
+  
+    /**
+     * amplitude:
+     * Returns the original Amplitude object.
+     */
+    amplitude: function () {
+      if (arguments.length > 0) {
+        amplitude.apply(this, arguments);
+        if (_debug) {
+          log('called amplitude(\'arguments\');');
+          log('with arguments: ' + JSON.stringify([].slice.apply(arguments)));
+        }
+        return;
+      }
+  
+      return amplitude;
+    },
+  
+    /**
+     * event:
+     * Event tracking
+     * @param eventName {String} required
+     * @param eventProperties {Object} optional
+     */
+    event: function (eventName, eventProperties) {
+      if(!eventName) {
+        warn('event name is required');
+        return;
+      }
+  
+      amplitude.getInstance().logEvent(eventName, eventProperties);
+    },
+  
+    /**
+     * resetUserId:
+     * 
+     */
+    resetUserId: function () {
+      amplitude.getInstance().setUserId(null);
+      amplitude.getInstance().regenerateDeviceId();
+    },
+  
+    /**
+     * setUserId:
+     * 
+     * @param userID {String} required
+     */
+    setUserId: function (userID) {
+      if(!userID) {
+        warn('userID is required');
+        return;
+      }
+  
+      amplitude.getInstance().setUserId(userID);
     }
+  };
+}
 
-    return amplitude;
-  },
-
-  /**
-   * event:
-   * Event tracking
-   * @param eventName {String} required
-   * @param eventProperties {Object} optional
-   */
-  event: function (eventName, eventProperties) {
-    if(!eventName) {
-      warn('event name is required');
-      return;
-    }
-
-    amplitude.getInstance().logEvent(eventName, eventProperties);
-  },
-
-  /**
-   * resetUserId:
-   * 
-   */
-  resetUserId: function () {
-    amplitude.getInstance().setUserId(null);
-    amplitude.getInstance().regenerateDeviceId();
-  },
-
-  /**
-   * setUserId:
-   * 
-   * @param userID {String} required
-   */
-  setUserId: function (userID) {
-    if(!userID) {
-      warn('userID is required');
-      return;
-    }
-
-    amplitude.getInstance().setUserId(userID);
-  }
-};
 
 export default Amplitude;
